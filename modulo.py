02aefcf92a09916ed994d3a7169855eabb518331
Kevin Sanchez <100317309+kevsanz12@users.noreply.github.com>
2023-01-19 17:11:37 -0300

    Add files via upload
    En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas de una calculadora: sumar, restar, multiplicar y dividir.
    
    Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. Los resultados tenéis que mostrarlos por consola.

---
+++ modulo.py
@@ -0,0 +1,9 @@
+from operaciones import *
+
+a = int(input("Ingresar primer numero: ")) 
+b = int(input("Ingresar segundo numero: "))
+
+print(suma(a, b))
+print(resta(a, b))
+print(multi(a, b))
+print(division(a, b))
---
+++ operaciones.py
@@ -0,0 +1,15 @@
+def suma(a, b):
+    resSuma = a + b
+    return resSuma
+
+def resta(a, b):
+    resResta = a - b
+    return resResta
+
+def multi(a, b):
+    resMulti = a * b
+    return resMulti
+
+def division(a, b):
+    resDiv = a / b
+    return resDiv
